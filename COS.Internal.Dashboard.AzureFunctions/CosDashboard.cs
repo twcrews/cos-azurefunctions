414da0802a165b8759bd727570db753b467b9d84
twcrews <twcrews96@gmail.com>
2023-07-09 17:00:09 -0500

    Implemented dependency-injected HTTP client.

--- COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
+++ COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
@@ -6,41 +6,25 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
-using System.Collections;
-using System.Text;
 using System.Globalization;
+using System.Collections;
 
 namespace COS.Internal.Dashboard.AzureFunctions
 {
 	public class CosDashboard
 	{
-		private static readonly HttpClient _client = new();
-		private const string FunctionName = "CosDashboard";
-		private readonly string _appID;
-		private readonly string _secret;
+		private readonly HttpClient _client;
+		public CosDashboard(IHttpClientFactory factory) => _client = factory.CreateClient(Constants.HttpClientName);
 
-		public CosDashboard()
-		{
-			IDictionary variables = Environment.GetEnvironmentVariables();
-
-			_appID = variables["PLANNING_CENTER_APP_ID"] as string;
-			_secret = variables["PLANNING_CENTER_SECRET"] as string;
-
-			string encodedCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_appID}:{_secret}"));
-			_client.DefaultRequestHeaders.Authorization = new("Basic", encodedCredentials);
-
-			_client.BaseAddress = new(variables["PLANNING_CENTER_API_URL"] as string);
-		}
-
-		[FunctionName(FunctionName)]
+		[FunctionName(Constants.DashboardFunctionName)]
 		public async Task<IActionResult> Run(
-			[HttpTrigger(AuthorizationLevel.Function, "get", Route = $"{FunctionName}/{{*remainder}}")] HttpRequest request,
+			[HttpTrigger(AuthorizationLevel.Function, "get", Route = Constants.DashboardFunctionRoute)] HttpRequest request,
 			ILogger log)
 		{
 			try
 			{
 				string path = request.Path.Value
-					.Replace($"/api/{FunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
+					.Replace($"/api/{Constants.DashboardFunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
 				log.LogInformation($"Processed a request to the following path: {path}");
 
 				HttpResponseMessage response = await _client.GetAsync(path);
@@ -50,12 +34,18 @@ namespace COS.Internal.Dashboard.AzureFunctions
 			} 
 			catch (Exception exception)
 			{
-				string message = exception.Message
-					.Replace(_appID, "*****", true, CultureInfo.InvariantCulture)
-					.Replace(_secret, "*****", true, CultureInfo.InvariantCulture);
+				IDictionary variables = Environment.GetEnvironmentVariables();
+				string appID = variables[Constants.PlanningCenterAppID] as string;
+				string secret = variables[Constants.PlanningCenterSecret] as string;
 
-				ObjectResult response = new(message);
-				response.StatusCode = StatusCodes.Status500InternalServerError;
+				string message = exception.Message
+					.Replace(appID, "*****", true, CultureInfo.InvariantCulture)
+					.Replace(secret, "*****", true, CultureInfo.InvariantCulture);
+
+				ObjectResult response = new(message)
+				{
+					StatusCode = StatusCodes.Status500InternalServerError
+				};
 				return response;
 			}
 		}
---
+++ COS.Internal.Dashboard.AzureFunctions/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(COS.Internal.Dashboard.AzureFunctions.Startup))]
+namespace COS.Internal.Dashboard.AzureFunctions;
+
+public class Startup : FunctionsStartup
+{
+	public override void Configure(IFunctionsHostBuilder builder)
+	{
+		builder.Services.AddHttpClient(Constants.HttpClientName, client =>
+		{
+			IDictionary variables = Environment.GetEnvironmentVariables();
+
+			string appID = variables[Constants.PlanningCenterAppID] as string;
+			string secret = variables[Constants.PlanningCenterSecret] as string;
+			string encodedCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{appID}:{secret}"));
+
+			client.BaseAddress = new(variables[Constants.PlanningCenterApiUrl] as string);
+			client.DefaultRequestHeaders.Authorization = new("Basic", encodedCredentials);
+		});
+	}
+}
