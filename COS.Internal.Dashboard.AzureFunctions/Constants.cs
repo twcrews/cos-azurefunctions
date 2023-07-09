a13faca273999f9232a3794834c53fe97a1b06b5
twcrews <twcrews96@gmail.com>
2023-07-09 16:54:44 -0500

    Added constants class.

---
+++ COS.Internal.Dashboard.AzureFunctions/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace COS.Internal.Dashboard.AzureFunctions;
+
+public static class Constants
+{
+	public const string PlanningCenterAppID = "PLANNING_CENTER_APP_ID";
+	public const string PlanningCenterSecret = "PLANNING_CENTER_SECRET";
+	public const string PlanningCenterApiUrl = "PLANNING_CENTER_API_URL";
+}
