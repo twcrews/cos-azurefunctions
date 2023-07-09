3dd4c437d394f8943a720bf552ccace6c2dc3c01
twcrews <twcrews96@gmail.com>
2023-07-09 16:59:19 -0500

    Added more constants.

--- COS.Internal.Dashboard.AzureFunctions/Constants.cs
+++ COS.Internal.Dashboard.AzureFunctions/Constants.cs
@@ -5,4 +5,9 @@ public static class Constants
 	public const string PlanningCenterAppID = "PLANNING_CENTER_APP_ID";
 	public const string PlanningCenterSecret = "PLANNING_CENTER_SECRET";
 	public const string PlanningCenterApiUrl = "PLANNING_CENTER_API_URL";
+
+	public const string HttpClientName = "DefaultClient";
+
+	public const string DashboardFunctionName = "CosDashboard";
+	public const string DashboardFunctionRoute = $"{DashboardFunctionName}/{{*remainder}}";
 }
