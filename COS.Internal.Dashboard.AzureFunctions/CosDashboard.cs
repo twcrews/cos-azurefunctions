ba62923ea4d2c23ab0553c08ff7646641fa62fff
twcrews <twcrews96@gmail.com>
2023-07-09 15:57:32 -0500

    Added exception message handling.

--- COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
+++ COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Collections;
 using System.Text;
+using System.Globalization;
 
 namespace COS.Internal.Dashboard.AzureFunctions
 {
@@ -15,15 +16,17 @@ namespace COS.Internal.Dashboard.AzureFunctions
 	{
 		private static readonly HttpClient _client = new();
 		private const string FunctionName = "CosDashboard";
+		private readonly string _appID;
+		private readonly string _secret;
 
 		public CosDashboard()
 		{
 			IDictionary variables = Environment.GetEnvironmentVariables();
 
-			string appID = variables["PLANNING_CENTER_APP_ID"] as string;
-			string secret = variables["PLANNING_CENTER_SECRET"] as string;
+			_appID = variables["PLANNING_CENTER_APP_ID"] as string;
+			_secret = variables["PLANNING_CENTER_SECRET"] as string;
 
-			string encodedCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{appID}:{secret}"));
+			string encodedCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_appID}:{_secret}"));
 			_client.DefaultRequestHeaders.Authorization = new("Basic", encodedCredentials);
 
 			_client.BaseAddress = new(variables["PLANNING_CENTER_API_URL"] as string);
@@ -34,14 +37,27 @@ namespace COS.Internal.Dashboard.AzureFunctions
 			[HttpTrigger(AuthorizationLevel.Function, "get", Route = $"{FunctionName}/{{*remainder}}")] HttpRequest request,
 			ILogger log)
 		{
-			string path = request.Path.Value
-				.Replace($"/api/{FunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
-			log.LogInformation($"Processed a request to the following path: {path}");
+			try
+			{
+				string path = request.Path.Value
+					.Replace($"/api/{FunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
+				log.LogInformation($"Processed a request to the following path: {path}");
 
-			HttpResponseMessage response = await _client.GetAsync(path);
-			string body = await response.Content.ReadAsStringAsync();
+				HttpResponseMessage response = await _client.GetAsync(path);
+				string body = await response.Content.ReadAsStringAsync();
 
-			return new OkObjectResult(body);
+				return new OkObjectResult(body);
+			} 
+			catch (Exception exception)
+			{
+				string message = exception.Message
+					.Replace(_appID, "*****", true, CultureInfo.InvariantCulture)
+					.Replace(_secret, "*****", true, CultureInfo.InvariantCulture);
+
+				ObjectResult response = new(message);
+				response.StatusCode = StatusCodes.Status500InternalServerError;
+				return response;
+			}
 		}
 	}
 }
