44c6f5a2735594e39b451132bdf1c478c35b1d0f
twcrews <twcrews96@gmail.com>
2023-07-09 22:20:23 -0500

    Refactored to use file-level namespace.

--- COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
+++ COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
@@ -9,45 +9,44 @@ using System.Net.Http;
 using System.Globalization;
 using System.Collections;
 
-namespace COS.Internal.Dashboard.AzureFunctions
+namespace COS.Internal.Dashboard.AzureFunctions;
+
+public class CosDashboard
 {
-	public class CosDashboard
+	private readonly HttpClient _client;
+	public CosDashboard(IHttpClientFactory factory) => _client = factory.CreateClient(Constants.HttpClientName);
+
+	[FunctionName(Constants.DashboardFunctionName)]
+	public async Task<IActionResult> Run(
+		[HttpTrigger(AuthorizationLevel.Function, "get", Route = Constants.DashboardFunctionRoute)] HttpRequest request,
+		ILogger log)
 	{
-		private readonly HttpClient _client;
-		public CosDashboard(IHttpClientFactory factory) => _client = factory.CreateClient(Constants.HttpClientName);
-
-		[FunctionName(Constants.DashboardFunctionName)]
-		public async Task<IActionResult> Run(
-			[HttpTrigger(AuthorizationLevel.Function, "get", Route = Constants.DashboardFunctionRoute)] HttpRequest request,
-			ILogger log)
+		try
 		{
-			try
+			string path = request.Path.Value
+				.Replace($"/api/{Constants.DashboardFunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
+			log.LogInformation($"Processed a request to the following path: {path}");
+
+			HttpResponseMessage response = await _client.GetAsync(path);
+			string body = await response.Content.ReadAsStringAsync();
+
+			return new OkObjectResult(body);
+		} 
+		catch (Exception exception)
+		{
+			IDictionary variables = Environment.GetEnvironmentVariables();
+			string appID = variables[Constants.PlanningCenterAppID] as string;
+			string secret = variables[Constants.PlanningCenterSecret] as string;
+
+			string message = exception.Message
+				.Replace(appID, "*****", true, CultureInfo.InvariantCulture)
+				.Replace(secret, "*****", true, CultureInfo.InvariantCulture);
+
+			ObjectResult response = new(message)
 			{
-				string path = request.Path.Value
-					.Replace($"/api/{Constants.DashboardFunctionName}/", "", StringComparison.InvariantCultureIgnoreCase);
-				log.LogInformation($"Processed a request to the following path: {path}");
-
-				HttpResponseMessage response = await _client.GetAsync(path);
-				string body = await response.Content.ReadAsStringAsync();
-
-				return new OkObjectResult(body);
-			} 
-			catch (Exception exception)
-			{
-				IDictionary variables = Environment.GetEnvironmentVariables();
-				string appID = variables[Constants.PlanningCenterAppID] as string;
-				string secret = variables[Constants.PlanningCenterSecret] as string;
-
-				string message = exception.Message
-					.Replace(appID, "*****", true, CultureInfo.InvariantCulture)
-					.Replace(secret, "*****", true, CultureInfo.InvariantCulture);
-
-				ObjectResult response = new(message)
-				{
-					StatusCode = StatusCodes.Status500InternalServerError
-				};
-				return response;
-			}
+				StatusCode = StatusCodes.Status500InternalServerError
+			};
+			return response;
 		}
 	}
 }
