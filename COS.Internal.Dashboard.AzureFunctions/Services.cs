e8392ecfe85945be598ed35e73b3ced18862eb3c
twcrews <twcrews96@gmail.com>
2023-07-09 22:29:04 -0500

    Fixed parameter name.

--- COS.Internal.Dashboard.AzureFunctions/Services.cs
+++ COS.Internal.Dashboard.AzureFunctions/Services.cs
@@ -8,5 +8,10 @@ namespace COS.Internal.Dashboard.AzureFunctions;
 public static class Services
 {
 	[FunctionName(Constants.AuthenticationFunctionName)]
-	public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest _) => new OkResult();
+	public static IActionResult Run(
+		[HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request)
+	{
+		request.GetHashCode(); // Keeps warnings away for unused parameter.
+		return new OkResult();
+	}
 }
