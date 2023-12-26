82849fd463485ad64a51297b477f2275769c2455
Tommy Crews <tommy@crews.dev>
2023-12-25 20:56:52 -0600

    Added ignored directory for local data mocking.

--- COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
+++ COS.Internal.Dashboard.AzureFunctions/CosDashboard.cs
@@ -11,7 +11,6 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using System.Text.Json;
 
 namespace COS.Internal.Dashboard.AzureFunctions;
